73bfaafee48c6d14fbf7e977af4a785af25b5229
ABS104a <koukimiya12@gmail.com>
2014-11-30 15:40:23 +0900

    FirstCommit
    最初です．

---
+++ src/com/abs104a/mperwithsideproject/MainActivity.java
@@ -0,0 +1,33 @@
+package com.abs104a.mperwithsideproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity{
+
+	public final static String TAG = "MainActivity";
+	
+	//自分自身のActivity
+	private final Activity mActivity = this;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO MainServiceを起動する
+		super.onCreate(savedInstanceState);
+		
+		//MainServiceを起動する
+		Intent mainIntent = new Intent(mActivity,MainService.class);
+		mActivity.startService(mainIntent);
+		
+		Log.v(TAG,"ActivityStart!");
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Activityを終了させる
+		super.onStart();
+		finish();
+	}
+}
---
+++ src/com/abs104a/mperwithsideproject/MainService.java
@@ -0,0 +1,62 @@
+package com.abs104a.mperwithsideproject;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.view.WindowManager;
+
+/**
+ * メイン画面を表示するService
+ * 
+ * WindowManagerからにViewを登録して
+ * 画面上に重畳表示する．
+ * 
+ * 機能として
+ * ・プレイリストリスト表示		（タブ
+ * ・簡易アルバムリスト表示　	（タブ
+ * ・音楽の詳細表示
+ * ・再生・一時停止などの操作
+ * ・音量操作
+ * ・シーク
+ * ・ランダム&リピート
+ * ・リスト表示画面の遷移ボタン
+ * ・設定画面への遷移ボタン
+ * 
+ * 基本的な操作はこちらでカバーさせる．
+ * 
+ * @author Kouki
+ *
+ */
+public class MainService extends Service{
+	
+	//自分のサービス（Context取得用)
+	private final Service mService = this;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	/**
+	 * Serviceが開始された時
+	 */
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		WindowManager mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+		
+	}
+
+	/**
+	 * Serviceが終了した時
+	 */
+	@Override
+	public void onDestroy() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onDestroy();
+	}
+
+}
