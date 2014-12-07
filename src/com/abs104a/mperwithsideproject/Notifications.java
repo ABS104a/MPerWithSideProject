c1955537d3961b5bb36c9674f6f976467339b074
ABS104a <koukimiya12@gmail.com>
2014-12-07 20:09:24 +0900

    add Class "Notifications"

---
+++ src/com/abs104a/mperwithsideproject/Notifications.java
@@ -0,0 +1,13 @@
+package com.abs104a.mperwithsideproject;
+
+/**
+ * 通知バーへの通知機能を提供するクラス
+ * @author Kouki-Mobile
+ *
+ */
+public final class Notifications {
+
+	public final void initNotifition(){
+		//TODO 通知機能の初期化
+	}
+}
