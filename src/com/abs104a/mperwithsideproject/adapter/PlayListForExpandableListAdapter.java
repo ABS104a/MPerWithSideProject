02599588b4bba167e3fe4456a4e0a9f5ab0a47e7
ABS104a-mobile <koukimiya12@gmail.com>
2014-12-13 12:59:35 +0900

    rename new adapter

--- src/com/abs104a/mperwithsideproject/adapter/PlayListForExpandableListAdapter.java
+++ src/com/abs104a/mperwithsideproject/adapter/PlayListForExpandableListAdapter.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 
-public class PlayListForExpandableListAdapte extends
+public class PlayListForExpandableListAdapter extends
 	BaseExpandableListAdapter {
 	
 	private static final int LAYOUT = 1; //TODO
