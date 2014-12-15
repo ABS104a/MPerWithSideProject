494dfc73c93c2a2de991b09242186d172d082f4b
ABS104a <koukimiya12@gmail.com>
2014-12-16 00:07:35 +0900

    add toast

--- src/com/abs104a/mperwithsideproject/viewctl/listener/ShuffleButtonOnClickImpl.java
+++ src/com/abs104a/mperwithsideproject/viewctl/listener/ShuffleButtonOnClickImpl.java
@@ -1,10 +1,12 @@
 package com.abs104a.mperwithsideproject.viewctl.listener;
 
+import com.abs104a.mperwithsideproject.R;
 import com.abs104a.mperwithsideproject.music.MusicPlayerWithQueue;
 
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * シャッフルボタンが押された時に呼ばれるリスナImpl
@@ -36,10 +38,12 @@ public final class ShuffleButtonOnClickImpl implements OnClickListener {
 	public void onClick(View v) {
 		boolean isShuffle = _mpwpl.setShuffle(!_mpwpl.isShuffle());
 		if(isShuffle){
+			Toast.makeText(v.getContext(), R.string.shuffle_on, Toast.LENGTH_SHORT).show();
 			//シャッフルがONの時
 			//Viewへの反映
 		}else{
 			//シャッフルがOFFの時
+			Toast.makeText(v.getContext(), R.string.shuffle_off, Toast.LENGTH_SHORT).show();
 		}
 	}
 
