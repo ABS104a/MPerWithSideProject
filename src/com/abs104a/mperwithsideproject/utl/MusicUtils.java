c468a67b5ac68d2eee06ba455f08d82af6109855
ABS104a-lab <koukimiya12@gmail.com>
2014-12-24 18:00:57 +0900

    add notification

--- src/com/abs104a/mperwithsideproject/utl/MusicUtils.java
+++ src/com/abs104a/mperwithsideproject/utl/MusicUtils.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import com.abs104a.mperwithsideproject.MusicPlayerReceiver;
 import com.abs104a.mperwithsideproject.R;
 import com.abs104a.mperwithsideproject.adapter.MusicViewPagerAdapter;
 import com.abs104a.mperwithsideproject.music.Album;
@@ -15,22 +14,14 @@ import com.abs104a.mperwithsideproject.viewctl.MusicSeekBarHandler;
 import com.abs104a.mperwithsideproject.viewctl.ViewPagerForEqualizerViewCtl;
 import com.abs104a.mperwithsideproject.viewctl.listener.MusicSeekBarOnChangeImpl;
 
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.AudioManager;
-import android.media.MediaMetadataRetriever;
-import android.media.MediaPlayer;
-import android.media.RemoteControlClient;
 import android.net.Uri;
-import android.os.PowerManager;
 import android.provider.MediaStore;
 import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
@@ -369,64 +360,6 @@ public class MusicUtils {
 		mHandler = new MusicSeekBarHandler(currentTime,seekbar,mpwpl);
 		mHandler.sleep(0);
 		
-	}
-	
-	/**
-	 * Lockスクリーンに通知を表示させるメソッド
-	 * @param mPlayer
-	 * @param mContext
-	 */
-	public final static void setLockScreenNotifition(final MediaPlayer mPlayer,final Context mContext){
-		MusicPlayerWithQueue mpwpl = getMusicController(mContext);
-		Music item = mpwpl.getNowPlayingMusic();
-		if(mpwpl.getNowPlayingMusic() == null)
-			return;
-		
-		AudioManager mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
-
-		// setWakeModeを以下のように呼び出す
-		mPlayer.setWakeMode(mContext.getApplicationContext(),
-		    PowerManager.PARTIAL_WAKE_LOCK);
-		 
-		// Intent.ACTION_MEDIA_BUTTONのブロードキャストを受け取る
-		// BroadcastReceiverでComponentNameを生成
-		ComponentName mMediaButtonReceiverComponent =
-		    new ComponentName(mContext, MusicPlayerReceiver.class); // ※
-		// AudioManagerにComponentNameを登録
-		mAudioManager.registerMediaButtonEventReceiver(mMediaButtonReceiverComponent);
-		 
-		// Intent.ACTION_MEDIA_BUTTONをアクションに持つIntentを生成
-		Intent intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
-		// ComponentNameをIntentに設定
-		intent.setComponent(mMediaButtonReceiverComponent);
-		// RemoteControlClientを生成
-		RemoteControlClient mRemoteControlClient = new RemoteControlClient(
-		    PendingIntent.getBroadcast(mContext, 0 , intent, 0));
-		// AudioManagerにRemoteControlClientを登録
-		mAudioManager.registerRemoteControlClient(mRemoteControlClient);
-		 
-		// リモコンの状態を設定
-		mRemoteControlClient.setPlaybackState(RemoteControlClient.PLAYSTATE_PLAYING);
-		// リモコン上で扱える操作を設定
-		mRemoteControlClient.setTransportControlFlags(
-		    RemoteControlClient.FLAG_KEY_MEDIA_PLAY
-		    | RemoteControlClient.FLAG_KEY_MEDIA_PAUSE
-		    | RemoteControlClient.FLAG_KEY_MEDIA_NEXT
-		    | RemoteControlClient.FLAG_KEY_MEDIA_STOP);
-		
-		Bitmap mArbumArt;
-		if(ImageCache.isCache(item.getAlbumUri().toString()))
-			mArbumArt = ImageCache.getImage(item.getAlbumUri().toString());
-		else
-			mArbumArt = null;
-		
-		// リモコン上の曲情報を更新
-		mRemoteControlClient.editMetadata(true)
-		    .putString(MediaMetadataRetriever.METADATA_KEY_ARTIST, item.getArtist())
-		    .putString(MediaMetadataRetriever.METADATA_KEY_ALBUM, item.getAlbum())
-		    .putString(MediaMetadataRetriever.METADATA_KEY_TITLE, item.getTitle())
-		    .putLong(MediaMetadataRetriever.METADATA_KEY_DURATION, item.getDuration())
-		    .putBitmap(RemoteControlClient.MetadataEditor.BITMAP_KEY_ARTWORK, mArbumArt).apply();
 	}
 	
 	
