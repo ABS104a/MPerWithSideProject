c524e6ff552b8cb3e117940b4f714197e1ed353f
ABS104a-lab <koukimiya12@gmail.com>
2015-01-18 17:35:39 +0900

    change row order

--- src/com/abs104a/mperwithsideproject/Column.java
+++ src/com/abs104a/mperwithsideproject/Column.java
@@ -2,9 +2,9 @@ package com.abs104a.mperwithsideproject;
 
 public class Column {
 
-	public static final int QUEUE = 0;
-	public static final int PLAYLIST = 1;
-	public static final int ALBUM = 2;
+	public static final int QUEUE = 1;
+	public static final int PLAYLIST = 2;
+	public static final int ALBUM = 0;
 	public static final int EQUALIZER = 3;
 	
 }
